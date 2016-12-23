cce09cf23c5bd03108c0249b246c11fd6b894f4e
Ramkumar P <ramkumarp@godbtech.com>
2016-12-23 11:22:03 +0530

    bat files check email and salesforce queries

---
+++ assets/bat/Email.bat
@@ -0,0 +1,5 @@
+echo off
+PATH=%PATH%;D:\shush\php\
+CD F:\ApacheBK\ApacheHtdocs\api\php\export\
+php index.php compare email
+exit
---
+++ assets/bat/SalesForce.bat
@@ -0,0 +1,5 @@
+echo off
+PATH=%PATH%;D:\shush\php\
+CD D:\shush\Apache24\htdocs\ramkumarp\export\
+php index.php compare salesforce
+exit
