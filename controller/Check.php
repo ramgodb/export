f2652c231ea5649c3e325e796634d570ac45625e
Ramkumar P <ramkumarp@godbtech.com>
2017-01-27 19:51:06 +0530

    apache checking

--- controller/Check.php
+++ controller/Check.php
@@ -14,5 +14,25 @@ class controlCheck
 		print_r($res);
 		echo "</pre>";
 	}
+	public function apache() {
+		$pageurl = HTTP_PATH;
+		$filename = "information.php";
+
+		$theurl = $pageurl.$filename;
+
+		$headers = array(
+		    'X-Requested-With: XMLHttpRequest',
+		);
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $theurl);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_TIMEOUT,30);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+		$exec = curl_exec($ch);
+		$info = curl_getinfo($ch);
+		var_dump($info);
+	}
 }
 ?>
