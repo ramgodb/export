586f11b43bdd5e1da9f6fc237cdc354efe9d9147
Ramkumar P <ramkumarp@godbtech.com>
2017-01-27 20:27:40 +0530

    apache checking

--- controller/Check.php
+++ controller/Check.php
@@ -1,6 +1,10 @@
 <?php
 class controlCheck
 {
+	public function __construct() {
+		$this->emailTo = SITE_EMAIL; 
+		$this->emailCc = SITE_EMAIL_SUPPORT;
+	}
 	public function info() {
 		echo "Working fine";
 	}
@@ -13,6 +17,15 @@ class controlCheck
 		echo "<pre>";
 		print_r($res);
 		echo "</pre>";
+	}
+	private function sendMail($subject, $message) {
+		$cc = $this->emailCc; 
+		$to = $this->emailTo;
+		$mail = new libMail();
+		if($mail->send_email(null,$to,$subject,$message,$cc))
+			return true;
+		else
+			return false;
 	}
 	public function apache() {
 		$pageurl = "http://jayakaranv:Password29@192.168.101.80:84/export/information.php";
@@ -34,7 +47,9 @@ class controlCheck
 		$info = curl_getinfo($ch);
 		
 		if($info['http_code'] != 200) {
-			
+			$msg = "<div>Apache failed....</div>";
+			$subject = 	APP ." - Apche ping failed";
+			$mail = $this->sendMail($subject, $msg);
 		}
 	}
 }
