8b258c67d56d73a1272da1ff2e17c47b75851620
Vetri <vetrivel@godbtech.com>
2017-04-27 11:52:29 +0530

    new query update for BVM

--- model/List.php
+++ model/List.php
@@ -119,9 +119,10 @@ class modelList extends libDatabase
 	protected function listData() {
 		if (PHP_SAPI === 'cli') 
 			fwrite(STDERR, "Generating data in model listData()...\r\n");
-		/*$bvm_qry = "SELECT param_value FROM M_PARAM WHERE param_name = 'BVM_LIST_VAL'";
+
+		$bvm_qry = "SELECT param_value FROM M_PARAM WHERE param_name = 'BVM_LIST_VAL'";
 		$bvm_row = $this->fetch_assoc($bvm_qry);
-		$bvm_param_name = $bvm_row[0]['param_value'];*/
+		$bvm_param_name = $bvm_row[0]['param_value'];
 		/*$mappQry = "SELECT
 						UD.cia_user_id,
 						S.HREmplID AS analyst_id,
